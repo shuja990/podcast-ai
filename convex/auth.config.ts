2a645eb6f92f4bb2584e34356c176ef818e2f359
Shuja Ali <shujaali1234@gmail.com>
2024-07-12 01:10:34 +0500

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://prepared-beagle-91.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
